f750761cf170f75b710f2dfd998ae8d2d1cde095
hottabxp <live@localhost.localdomain>
2014-05-18 11:23:51 +0300

    Добавлен русский текст

--- vk_iphone.py
+++ vk_iphone.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 #https://oauth.vk.com/authorize?client_id=3682744&v=5.7&scope=wall,offline&redirect_uri=http://oauth.vk.com/blank.html&display=page&response_type=token
 import urllib
-text = "Test message!!!"
+text = "Тестовое сообщение)"
 token = ""
 base = "https://api.vk.com/method/wall.post?owner_id=34254853&message="+text+"&v=5.0&access_token="+token+""
 page = urllib.urlopen(base).read()
