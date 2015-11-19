eaab3624856fd26296696fd38fd3dbdaff941086
hottabxp <hottabxp@list.ru>
2015-11-19 22:35:37 +0300

    Update vk_iphone.py

--- vk_iphone.py
+++ vk_iphone.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 #https://oauth.vk.com/authorize?client_id=3682744&v=5.7&scope=wall,offline&redirect_uri=http://oauth.vk.com/blank.html&display=page&response_type=token
-import urllib
-text = "Тестовое сообщение)"
+#!/usr/bin/env python3
+from urllib.request import Request,urlopen
+text = "Hello"
 token = ""
 base = "https://api.vk.com/method/wall.post?owner_id=34254853&message="+text+"&v=5.0&access_token="+token+""
-page = urllib.urlopen(base).read()
-print page
+page = urlopen(base).read()
+print (page)
